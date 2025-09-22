dd5e2c671cde431eb631755689be56b2e50d633a
FarrewX <a0987142276@gmail.com>
2025-09-22 18:24:49 +0700

    rag model

--- app/Http/Controllers/OllamaController.php
+++ app/Http/Controllers/OllamaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Http;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Plos;
 
 class OllamaController extends Controller
 {
@@ -12,15 +13,36 @@ class OllamaController extends Controller
     {
         $prompt = $request->input('prompt');
         $course = $request->input('course');
+        $num_clo = $request->input('numClo');
+        $select_plo = $request->input('selectClo');
+
+        $plo = Plos::all();
 
         $response = Http::post('http://localhost:11434/v1/completions', [
             'model' => 'elo_generator',
-            'prompt' => 'ต้องการทำการสอนในวิชาเรียน'.$course.' โดยมีเนื้้อหาการเรียนดังนี้ '.$prompt.
-                ' จงสร้างclo 3 ข้อ โดยแต่ละข้อมีรายละเอียดดังนี้ 1.clo ที่ได้มาตรงกับ PLOs ไหน 2.รายละเอียดของ clo 
-                3.สอดคล้องกับหลักจริยธรรมไดบ้าง 4.การประเมินผลเป็นอย่างไร 5ตัวชี้วัดที่ใช้ในการประเมินผลการเรียนรู้',
+            'prompt' => 'ต่อจากนี้ให้ออกแบบ CLO ของหนึ่งรายวิชาให้สอดคล้องกับ PLO ของหลักสูตร โดยใช้ข้อมูลหลักสูตร:
+                    ชื่อหลักสูตร: สาขาวิชาวิทยการคอมพิวเตอร์
+                    รายละเอียด PLO ของหลักสูตร:
+                    ' .$plo->map(function ($item) {
+                            return $item->plo.': '.$item->description;
+                        })->implode("\n").'
+                    ใช้ข้อมูลรายวิชา:
+                    ชื่อรายวิชา:'.$course.'
+                    Course description:'.$prompt.'
+
+                    เงื่อนไขการออกแบบ CLO:
+                    - จำนวน CLO ที่ต้องการ:'.$num_clo.'
+                    - เลือกให้ CLO เหล่านี้สอดคล้องกับ PLO เฉพาะที่ต้องการ:'. implode(', ', (array)$select_plo) .'
+                    - รูปแบบการเขียน CLO: Action Verb - Object - Qualification Phase (ตามหลัก SMART)
+                    - Learning’s Level ของ CLO สามารถเท่ากับหรือต่ำกว่า PLO ที่สอดคล้อง
+                    - ต้องกำหนด Domain ของ CLO ให้ตรงกับ Domain ของ PLO
+                    - ต้องเสนอ Assessment Method ที่เหมาะสมกับ Learning’s Level
+                    - ต้องอธิบายเหตุผลประกอบการกำหนด Learning’s Level และ Assessment Method
+
+                    **รูปแบบคำตอบที่ต้องการ:**
+                    ตารางที่ประกอบด้วย:
+                    | CLO | PLO ที่รองรับ | Domain | Learning’s Level | Assessment Method | เหตุผล |',
         ]);
-        
-        $result = $response->json();
 
         return response()->json($response->json());
     }
